00efedbc422eee85557bbb8569f72c1bac02e3f7
Sebastian Mayorga <sebasmayorga98@gmail.com>
2020-09-14 15:18:52 -0500

    referencias

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
     <div class="container d-flex align-items-center">
 
       <div class="logo mr-auto">
-        <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>
+        <a href="index.php"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>
       </div>
 
       <nav class="nav-menu d-none d-lg-block">
