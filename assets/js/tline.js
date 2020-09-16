432d73d79d2249d65b3c405842620321ff6c4888
Sebastian Mayorga <sebasmayorga98@gmail.com>
2020-09-16 09:52:53 -0500

    cambiosLT

--- assets/js/tline.js
+++ assets/js/tline.js
@@ -1,34 +1,34 @@
-    $(".step").click( function() {
+$(".step").click( function() {
 	$(this).addClass("active").prevAll().addClass("active");
 	$(this).nextAll().removeClass("active");
 });
 
 $(".step01").click( function() {
 	$("#line-progress").css("width", "3%");
-	$("#contenedorS").css("background-image", "url(assets/img/conocenos/imagen1.png)");
+	$("#contenedorS").css("background-image", "url(./assets/img/conocenos/imagen1.jpg)");
 	$(".discovery").addClass("active").siblings().removeClass("active");
 });
 
 $(".step02").click( function() {
 	$("#line-progress").css("width", "25%");
-	$("#contenedorS").css("background-image", "url(assets/img/conocenos/imagen2.jpg)");
+	$("#contenedorS").css("background-image", "url(./assets/img/conocenos/imagen2.jpg)");
 	$(".strategy").addClass("active").siblings().removeClass("active");
 });
 
 $(".step03").click( function() {
 	$("#line-progress").css("width", "50%");
-	$("#contenedorS").css("background-image", "url(assets/img/conocenos/imagen3.png)");
+	$("#contenedorS").css("background-image", "url(./assets/img/conocenos/imagen3.jpg)");
 	$(".creative").addClass("active").siblings().removeClass("active");
 });
 
 $(".step04").click( function() {
 	$("#line-progress").css("width", "75%");
-	$("#contenedorS").css("background-image", "url(assets/img/conocenos/imagen4.jpg)");
+	$("#contenedorS").css("background-image", "url(./assets/img/conocenos/imagen4.jpg)");
 	$(".production").addClass("active").siblings().removeClass("active");
 });
 
 $(".step05").click( function() {
 	$("#line-progress").css("width", "100%");
-	$("#contenedorS").css("background-image", "url(assets/img/conocenos/imagen5.jpg)");
+	$("#contenedorS").css("background-image", "url(./assets/img/conocenos/imagen5.jpg)");
 	$(".analysis").addClass("active").siblings().removeClass("active");
 });
